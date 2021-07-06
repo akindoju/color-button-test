a9a9b95de15b09753a7ee74d3163ee54448fdf05
Akndju <davidd.akindoju@gmail.com>
2021-07-06 19:07:52 +0100

    Started test-based project with tdd approach

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct intial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //find an element with the role of button and text of 'change to blue'
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+
+  //expect bg color to be red
+  expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+
+  //click btn
+  fireEvent.click(colorBtn);
+
+  //expect bg color to be blue
+  expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
+
+  //expect bt text to be 'Change to red'
+  expect(colorBtn.textContent).toBe("Change to red");
 });
